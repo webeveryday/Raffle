699c283d8ede075ecd711164d33759eb0a8b8085
webeveryday <web3everyday@proton.me>
2024-09-14 15:54:05 -0400

    Add Entrance Fee

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -12,6 +12,21 @@ pragma solidity ^0.8.18;
  * @dev It implements Chainlink VRFv2 and Chainlink Automation
  */
 contract Raffe {
-    
+  // constant is the cheapest gas
+  // immutable is cheap gas
+  // i_ prefix for immutable variable
+  uint256 private immutable i_entranceFee;
 
+  constructor(uint256 entranceFee) {
+    i_entranceFee = entranceFee;
+  }
+  
+  function enterRaffle() public payable {}
+
+  function pickWinner() public {}
+
+  /** Getter Function */
+  function getEntranceFee() external view returns (uint256) {
+    return i_entranceFee;
+  }
 }
