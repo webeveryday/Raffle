bbc48626eeba8ad764aff4f8dae32809ede94b09
webeveryday <web3everyday@proton.me>
2024-09-15 16:24:15 -0400

    Example layout of the contract file

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,5 +1,28 @@
 // SPDX-License-Identifier: MIT
 
+// Layout of the contract file:
+// version
+// imports
+// errors
+// interfaces, libraries, contract
+// Inside Contract:
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+
+// external & public view & pure functions
+
 pragma solidity ^0.8.18;
 
 // This is NATSPEC
