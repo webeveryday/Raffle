15eb5de22422edcec1d3feddcf3e510685b672f4
webeveryday <web3everyday@proton.me>
2024-10-26 18:46:54 -0400

    Add Script

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract DeployRaffle is Script {
+  function run() public {}
+
+  function deployContract() public {}
+}
