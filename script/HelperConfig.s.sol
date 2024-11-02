9005bc695c23a0d15ff56c668c903d4d544e396c
webeveryday <web3everyday@proton.me>
2024-11-01 21:20:39 -0400

    Deploy a mock Chainlink VRF

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,8 +2,15 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2_5Mock} from "chainlink/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract CodeConstants {
+  // Pay certain amount of link token
+  // VRF Mock Values
+  uint96 public constant MOCK_BASE_FEE = 0.25 ether;
+  uint96 public constant MOCK_GAS_PRICE_LINK = 1e9;
+  int256 public constant MOCK_WEI_PER_UNIT_LINK = 4e15;    // LINK / ETH price
+  
   uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
   uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
@@ -31,7 +38,7 @@ contract HelperConfig is CodeConstants, Script {
     if (networkConfigs[chainId].vrfCoordinator != address(0)) {
       return networkConfigs[chainId];
     } else if (chainId == LOCAL_CHAIN_ID) {
-      // getOrCreateAnvilEthConfig()
+      return getOrCreateAnvilEthConfig();
     } else {
       revert HelperConfig__InvalidChainId();
     }
@@ -59,5 +66,33 @@ contract HelperConfig is CodeConstants, Script {
     });
   }
 
- 
+  function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+    // Check to see if we set an active network config
+    if (localNetworkConfig.vrfCoordinator != address(0)) {
+      return localNetworkConfig;
+    }
+
+    // Deploy mocks and such
+    vm.startBroadcast();
+
+    VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock(
+        MOCK_BASE_FEE,
+        MOCK_GAS_PRICE_LINK,
+        MOCK_WEI_PER_UNIT_LINK,
+    );
+
+    vm.stopBroadcast();
+
+    localNetworkConfig = NetworkConfig({
+        entranceFee: 0.01 ether,
+        interval: 30, // 30 seconds
+        vrfCoordinator: address(vrfCoordinatorMock),
+        // gasLane value doesn't matter.
+        gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+        subscriptionId: 0,
+        callbackGasLimit: 500000,
+    });
+
+    return localNetworkConfig;
+  }
 }
