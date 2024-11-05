2a0407116841f16cfea55b423799969c5f8bf52e
webeveryday <web3everyday@proton.me>
2024-11-05 17:36:52 -0500

    Add Raffle Test

---
+++ test/unit/RaffleTest.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployRaffle} from "script/DeployRaffle.s.sol";
+import {Raffle} from "src/Raffle.sol";
+
+contract RaffleTest is Test {}
