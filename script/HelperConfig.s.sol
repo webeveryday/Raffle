56667391bfd466142e11ef620920753f7a7d9231
webeveryday <web3everyday@proton.me>
2024-11-08 22:04:59 -0500

    Fix Error

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -82,7 +82,7 @@ contract HelperConfig is CodeConstants, Script {
     VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock(
         MOCK_BASE_FEE,
         MOCK_GAS_PRICE_LINK,
-        MOCK_WEI_PER_UNIT_LINK,
+        MOCK_WEI_PER_UNIT_LINK
     );
 
     vm.stopBroadcast();
@@ -94,7 +94,7 @@ contract HelperConfig is CodeConstants, Script {
         // gasLane value doesn't matter.
         gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
         subscriptionId: 0,
-        callbackGasLimit: 500000,
+        callbackGasLimit: 500000
     });
 
     return localNetworkConfig;
