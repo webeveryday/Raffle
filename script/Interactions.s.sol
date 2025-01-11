3305e56435e9904f65f51a950d5cdea5923a913f
webeveryday <web3everyday@proton.me>
2025-01-11 15:56:53 -0500

    Fund Subscription

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.18;
 
 import {Script, console} from "forge-std/Script.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
-
+import {HelperConfig, CodeConstants} from "./HelperConfig.s.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {LinkToken} from "test/mocks/LinkToken.sol";
 
 contract  CreateSubscription is Script {
   function createSubscriptionUsingConfig() public returns (uint256, address) {
@@ -29,5 +29,36 @@ contract  CreateSubscription is Script {
 
   function run() public {
     createSubscriptionUsingConfig();
+  }
+}
+
+contract FundSubscription is Script, CodeConstants {
+  uint256 public constant FUND_AMOUNT = 3 ether;    // We sending 3 Link instead of ETH
+
+  function fundSubscriptionUsingConfig() public {
+    HelperConfig helperConfig = new HelperConfig();
+    address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+    uint256 subscriptionId = helperConfig.getConfig().subscriptionId;
+
+  }
+
+  function fundSubscription(address vrfCoordinator, uint subscriptionId, address linkToken) public {
+    console.log("Funding subscription: ", subscriptionId);
+    console.log("Using vrfCoordinator: ", vrfCoordinator);
+    console.log("On ChainId: ", block.chainid);
+
+    if (block.chainid == LOCAL_CHAIN_ID) {
+      vm.startBroadcast();
+      VRFCoordinatorV2_5Mock(vrfCoordinator).fundSubscription(subscriptionId, FUND_AMOUNT);
+      vm.stopBroadcast();
+    } else {
+      vm.startBroadcast();
+      LinkToken(linkToken).transferAndCall(vrfCoordinator, FUND_AMOUNT, abi.encode(subscriptionId));
+      vm.stopBroadcast();
+    }
+  }
+
+  function run() public {
+    fundSubscriptionUsingConfig();
   }
 }
