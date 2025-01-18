fb73e746e4565c09d26e1012c3ee23daec235e3d
webeveryday <web3everyday@proton.me>
2025-01-17 20:37:02 -0500

    Test Perform Upkeep Can Only Run If Check Upkeep Is True

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -130,4 +130,18 @@ contract RaffleTest is Test {
     // Assert
     assert(!upkeepNeeded);
   }
+
+  /**
+  * PERFORM UPKEEP
+  */
+  function testPerformUpkeepCanOnlyRunIfCheckUpkeepIsTrue() public {
+    // Arrange
+    vm.prank(PLAYER);
+    raffle.enterRaffle{value: entranceFee}();
+    vm.warp(block.timestamp + interval  + 1);
+    vm.roll(block.number + 1);
+
+    // Act / Assert
+    raffle.performUpkeep("");
+  }
 }
