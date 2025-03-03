210444624265865547a47122cd18f6e581711125
webeveryday <web3everyday@proton.me>
2025-03-02 20:54:45 -0500

    Example of vm.recordLogs

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -5,6 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {DeployRaffle} from "script/DeployRaffle.s.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
+import {Vm} from "forge-std/Vm.sol";
 
 contract RaffleTest is Test {
   Raffle public raffle;
@@ -168,5 +169,26 @@ contract RaffleTest is Test {
     );
 
     raffle.performUpkeep("");
+  }
+
+  // Test for data from emitted events in our test
+  function testPerformUpkeepUpkeepUpdatesRaffleAndEmitsRequestId() public {
+    // Arrange
+    vm.prank(PLAYER);
+    raffle.enterRaffle{value: entranceFee}();
+    vm.warp(block.timestamp + interval  + 1);
+    vm.roll(block.number + 1);
+
+    // Act
+    // Keep track of event emit and put them into an array
+    vm.recordLogs();
+    raffle.performUpkeep("");
+    Vm.Log[] memory entries = vm.getRecordedLogs();   // This a special log
+    bytes32 requestId = entries[1].topics[1];
+
+    // Assert
+    Raffle.RaffleState raffleState = raffle.getRaffleState();
+    assert(uint256(requestId) > 0);     // Check if the request ID is not empty
+    assert(uint256(raffleState) == 1);
   }
 }
